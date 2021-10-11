e10c853e18b131613b04aa6d6e120339924de79e
Marrit Schellekens <marritschellekens@yahoo.com>
2021-10-11 21:28:52 +0200

    cleaned up file

--- smart_bike.py
+++ smart_bike.py
@@ -6,44 +6,35 @@ Created on Thu Apr 15 14:30:34 2021
 @author: marrit
 """
 
-"""trt_yolo.py
+"""smart_bike.py
 
-This script demonstrates how to do real-time object detection with
-TensorRT optimized YOLO engine.
+This script starts the smart_bike code, which detects upcoming vehicles using Yolo and a custom tracking algorithm. 
+
 """
+
+#Use the Camera device
+CAMERA = False
+#Write analysed frames to file. This makes the program run significantly slower.
+WRITE_TO_FILE = False
+#Video file to use if CAMERA is set to False
+VIDEO_FILE = "videos/d_raw.mp4" 
+#The simulated fps when analyzing a video file. Ignored when using camera. 
+SIMULATED_FPS = 6
+WINDOW_NAME = "tracking"
 
 import cv2
 from utils.camera import Camera
-
-
-
 import os
 import time
-
 from utils.display import open_window, set_display, show_fps
-
 import CentroidTracker
 import multiprocessing as mp
 import pycuda.autoinit #This is needed for intilazing CUDA driver
+#Originally the idea was that another file could be written that contained functions for my windows laptop. This could be imported here as an alternative to Jetson_functions. This idea never really came to fruition, but the structure remains. 
 import Jetson_functions as hw_functions
 import helper_functions as functions
 from utils.yolo_classes import get_cls_dict
 from utils.yolo_with_plugins import get_input_shape, TrtYOLO
-import shared_space_detector
-import roadTypeTracker
-#import VariableWriter
-
-
-
-CAMERA = False
-WRITE_TO_FILE = True
-VIDEO_FILE = "videos/d_raw.mp4" 
-SIMULATED_FPS = 6
-FPS_SS = 0.3
-WINDOW_NAME = "tracking"
-
-
-
 
  
 def loop_and_detect(cam, trt_yolo, classes, writer):
@@ -53,11 +44,8 @@ def loop_and_detect(cam, trt_yolo, classes, writer):
     framecounter = 0
 
     frameskip = round(6/SIMULATED_FPS)
-    frameskip_ss = round(6/FPS_SS)
     hw_func = hw_functions.jetson()
     tracker = CentroidTracker.CentroidTracker()
-    tracker_ss = roadTypeTracker.roadTypeTracker()
-    
     
     start = time.time()
     while True:
@@ -69,14 +57,10 @@ def loop_and_detect(cam, trt_yolo, classes, writer):
         if img is None:
             break
         
-        #if framecounter % frameskip_ss == 0:
-            #separated_bikepath = tracker_ss.update(img)
-            
-        
         if CAMERA:
-            tic, fps = update(tic, fps, img, trt_yolo, classes, tracker, hw_func, False, writer)
+            tic, fps = update(tic, fps, img, trt_yolo, classes, tracker, hw_func,  writer)
         elif framecounter % frameskip == 0:
-            tic, fps = update(tic, fps, img, trt_yolo, classes, tracker, hw_func, False, writer)
+            tic, fps = update(tic, fps, img, trt_yolo, classes, tracker, hw_func,  writer)
         
         
         framecounter+=1
@@ -91,21 +75,19 @@ def loop_and_detect(cam, trt_yolo, classes, writer):
             end = time.time()
 
  
-def update(tic, fps, img, trt_yolo, classes, tracker, hw_func, sep_path, writer):
-    tic, fps = functions.update_fps(tic, fps)
-    if not sep_path:
-        frame_detection, frame_tracking = functions.detect_and_track(img, trt_yolo, classes, tracker,hw_func)
-        frame_tracking = show_fps(frame_tracking, fps)
-        frame_tracking = cv2.resize(frame_tracking, (800, 600))
-    else:
-        frame_detection = img
-        frame_tracking = functions.drawTracking_separated(img)
-    cv2.imshow(WINDOW_NAME, frame_tracking)
-    if WRITE_TO_FILE:
-        writer.write(frame_tracking)
-    return tic, fps
+def update(tic, fps, img, trt_yolo, classes, tracker, hw_func, writer):
+	tic, fps = functions.update_fps(tic, fps)
+	frame_detection, frame_tracking = functions.detect_and_track(img, trt_yolo, classes, tracker,hw_func)
+	frame_tracking = show_fps(frame_tracking, fps)
+	frame_tracking = cv2.resize(frame_tracking, (800, 600))
+	cv2.imshow(WINDOW_NAME, frame_tracking)
+	if WRITE_TO_FILE:
+        	writer.write(frame_tracking)
+	return tic, fps
+
 
 def main():
+    #You don't need to pass arguments. I kind of hacked around that because I was too lazy to completely rewrite this part. 
     args = hw_functions.parse_args(VIDEO_FILE, CAMERA)
     if args.category_num <= 0:
         raise SystemExit('ERROR: bad category_num (%d)!' % args.category_num)
@@ -121,9 +103,6 @@ def main():
     trt_yolo = TrtYOLO(args.model, (h, w), args.category_num, args.letter_box)
 
     fourcc = cv2.VideoWriter_fourcc(*"MP4V")
-    #When using camera, and you want the file saved, adjust size parameters
-    #VIDEO_NAME = "/media/marrit/E61E-23E2/output_" + 'TIME' + ".mp4"
-    #VIDEO_NAME = "/home/marrit/project/tensorrt_demos/output_" + 'TIME' + ".mp4"
     VIDEO_NAME = "/home/marrit/Videos/output.mp4"
     frame_width = 800
     frame_height = 600
@@ -133,10 +112,8 @@ def main():
         writer = cv2.VideoWriter(VIDEO_NAME, cv2.VideoWriter_fourcc(*"mp4v"), SIMULATED_FPS, (frame_width, frame_height))
     else:
         writer = None
-    #varWriter = VariableWriter.VariableWriter(SIMULATED_FPS, writer)
     
     
-    #writer = cv2.VideoWriter('output.mp4', fourcc, SIMULATED_FPS, (800, 600), True)
     open_window(  
         WINDOW_NAME, 'Camera TensorRT YOLO Demo',
         cam.img_width, cam.img_height)
